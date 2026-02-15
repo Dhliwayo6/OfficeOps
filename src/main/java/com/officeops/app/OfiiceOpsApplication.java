eba64c49183169116c4b53df2f179308c188d683
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2026-02-15 17:01:56 +0200

    Initialize project

---
+++ src/main/java/com/officeops/app/OfiiceOpsApplication.java
@@ -0,0 +1,13 @@
+package com.officeops.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfiiceOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfiiceOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/officeops/app/OfiiceOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.officeops.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfiiceOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
